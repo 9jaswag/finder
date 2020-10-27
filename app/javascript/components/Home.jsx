582c58279743e6aad56d9fe871b783aa9520830b
Chuks Opia <chuks24ng@yahoo.co.uk>
2020-10-27 19:12:50 +0100

    UI refactor

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -6,8 +6,11 @@ export default () => {
   return (
     <div className="wrapper">
       <section className="hero">
-        <div className="form">
-          <Form />
+        <div className="content">
+          <h3>Search for users' email by their names and URL</h3>
+          <div className="form">
+            <Form />
+          </div>
         </div>
       </section>
       <main className="user-list">
